a7b9f83acf8c4d1d92bad8e20350471c0a1333fa
Caly.T <caly.trasibule@viacesi.fr>
2020-05-27 18:31:48 +0200

    ajout page erreur

---
+++ Views/erreur.php
@@ -0,0 +1,30 @@
+<?php
+ ob_start();
+ $stylesheets=["main","accueil"]
+ ?>
+
+<div class="container connectlife">
+    <div class="row">
+        <div class="head">
+            <img class="col-6" src="./Public/Images/connectLife.png" alt=logo>
+        </div>
+        <div class="col-12 d-flex flex-column align-items-center">
+            <div class= "head col titre1"> 
+                <p>Une erreur est survenue !</p>
+            </div>
+            <div class= "head col text">
+                <p>Nous sommes désolés #inserNOM/PRENOM mais il semble qu'une erreur ce soit produite.</p>
+                <p>L'adresse Mail renseingée ne correspond pas.</p> 
+                <p>Vous pouvez <strong class="strong">retourner au formulaire</strong> et renseingner de nouveau votre adresse Mail</p>  
+                <p>Ou, <strong class="strong">contactez notre service mass-mailing</strong> pour des informations complémentaires.</p> 
+            </div>
+            <div class="head">
+                <button class= "col btn"><a href= #>Formulaire</a></button>     
+                <button class= "col btn"><a href= #>Contact</a></button>         
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+$content=ob_get_clean();
+require("Views/template.php");
